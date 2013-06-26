e778ca9e64a23e74e42311a2bbdb55bf73ace533
Joe Glenn <joe.glenn@robotsandpencils.com>
2013-06-26 14:25:13 -0600

    moved row creation to its own function, fixed indentation and console.log's are now console.debug

--- js/rpstackmob.js
+++ js/rpstackmob.js
@@ -1,13 +1,13 @@
 /*****************************************************
- * Initialization, Types, and Global Variables
+ * Initialization and Type Declarations
  *****************************************************/
 
 /**
  * StackMob Init
  */
 StackMob.init({
-publicKey: '2109fd4e-35b1-4d0b-a6b8-be290fa0bd09',
-apiVersion : 0
+	publicKey: '2109fd4e-35b1-4d0b-a6b8-be290fa0bd09',
+	apiVersion : 0
 });
 
 /**
@@ -33,7 +33,7 @@ var Datas = StackMob.Collection.extend({
  * Validates the form and verifies required fields
  */
 function validateForm() {
-	 console.log("Validating form");
+	 console.debug("Validating form");
 	 /*
 	 .
 	 .
@@ -46,27 +46,27 @@ function validateForm() {
  */
 function submitForm() {
  
-var data = new Data({
-	singleline: document.getElementById("singleLine").value,
-	multiline:  document.getElementById("multiLine").value,
-	list:       document.getElementById("list").value,
-	radioone:   document.getElementById("radioOne").checked,
-	radiotwo:   document.getElementById("radioTwo").checked,
-	integer:    document.getElementById("integer").value,
-	date:       document.getElementById("date").value
-});//above keys must remain lowercase
-
-data.create({
-	success: function(model, result, options) {
-		//console.debug(model.toJSON());
-		document.getElementById("debugOutput").innerHTML = "<h3 class='text-success'>Success!</h3>";
-	},
-	error: function(model, error, options) {
-		//console.debug(error);
-		document.getElementById("debugOutput").innerHTML = "<h3 class='text-error'>Failure!</h3>";
-	}
-});
- }
+	var data = new Data({
+		singleline: document.getElementById("singleLine").value,
+		multiline:  document.getElementById("multiLine").value,
+		list:       document.getElementById("list").value,
+		radioone:   document.getElementById("radioOne").checked,
+		radiotwo:   document.getElementById("radioTwo").checked,
+		integer:    document.getElementById("integer").value,
+		date:       document.getElementById("date").value
+	});//above keys must remain lowercase
+	
+	data.create({
+		success: function(model, result, options) {
+			//console.debug(model.toJSON());
+			document.getElementById("debugOutput").innerHTML = "<h3 class='text-success'>Success!</h3>";
+		},
+		error: function(model, error, options) {
+			//console.debug(error);
+			document.getElementById("debugOutput").innerHTML = "<h3 class='text-error'>Failure!</h3>";
+		}
+	});
+}
 
 /**
  * refreshTable()
@@ -88,36 +88,44 @@ function refreshTable() {
 		
 		var array = collection.toJSON();
 
-	  	for (var i = 0; i < list.length; i++) {
-	  		var item = array[i];
-	  		
-	  		var str = "<tr>";
-	  		str += "<td id='" + item.data_id + "'><i class='icon-remove'></td>";
-	  		str += "<td>" + item.singleline + "</td>";
-	  		str += "<td>" + item.multiline + "</td>";
-	  		str += "<td>" + item.list + "</td>";
-	  		str += "<td>" + item.radioone + "</td>";
-	  		str += "<td>" + item.radiotwo + "</td>";
-	  		str += "<td>" + item.integer + "</td>";
-	  		str += "<td>" + item.date + "</td>";
-	  		str += "</tr>";
-	  		
-	  		document.getElementById("tableBody").innerHTML += str;
-	  		/* console.log(str); */
-		}
+	  	for (var i = 0; i < list.length; i++)
+	  		document.getElementById("tableBody").innerHTML += createRow(array[i]);
 		},
 		error: function() {
-  		document.getElementById("tableBody").innerHTML = "<h2 class='text-error'>Failured to refresh!</h2>"
+  			document.getElementById("tableBody").innerHTML = "<h2 class='text-error'>Failed to refresh..</h2>"
 		}
 	});
 }
 
 /**
- * Delete Row
- * Deletes the selected row from the table
+ * createRow(item)
+ * Creates a row for the table
+ * @return string of html markup for the row
+ */
+function createRow(item) {
+	var str = "<tr id='" + item.data_id + "'>";
+	
+	str += "<td> <i class='icon-remove'> </td>";
+	str += "<td>" + item.singleline   + "</td>";
+	str += "<td>" + item.multiline    + "</td>";
+	str += "<td>" + item.list         + "</td>";
+	str += "<td>" + item.radioone     + "</td>";
+	str += "<td>" + item.radiotwo     + "</td>";
+	str += "<td>" + item.integer      + "</td>";
+	str += "<td>" + item.date         + "</td>";
+	str += "</tr>";
+	
+	/* console.debug(str); */
+	return str;
+}
+
+
+/**
+ * deleteRow(id)
+ * Deletes the selected row from the table using the stackmob id
  */
 function deleteRow(id) {
-	 console.log("Deleting row with id of" + id);
+	 console.debug("Deleting row with id of" + id);
 	 /*
 	 .
 	 .
@@ -144,5 +152,5 @@ $(document).ready(function() {
 	$("#refreshButton").on("click", function() {
 		refreshTable();
 	});
-	
+
 });
